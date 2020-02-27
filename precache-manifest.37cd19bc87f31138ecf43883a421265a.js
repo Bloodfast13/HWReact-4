bde7052823c19918984ef47b24312c1f3d122a2b
Serg <bloodfast13@gmail.com>
2020-02-27 21:06:33 +0200

    Updates

---
+++ precache-manifest.37cd19bc87f31138ecf43883a421265a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32137b6fa5cb68acc0ecf1352185470",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c129d118a38368c483a",
+    "url": "./static/css/main.40c0c9cd.chunk.css"
+  },
+  {
+    "revision": "36a3d5e08472ad9fadd7",
+    "url": "./static/js/2.a2a62e26.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.a2a62e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c129d118a38368c483a",
+    "url": "./static/js/main.e2a7545f.chunk.js"
+  },
+  {
+    "revision": "e4e442ba85da7862a82a",
+    "url": "./static/js/runtime-main.0757bff1.js"
+  }
+]);
